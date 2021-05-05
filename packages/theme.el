513b93f4d62727b7be61f5edc640259f8b4ad8b7
panthevm <panthevm@yandex.ru>
2021-05-05 09:02:22 +0300

    Settings emacsclient font

--- packages/theme.el
+++ packages/theme.el
@@ -1,7 +1,12 @@
-(use-package habamax-theme
+(use-package gruvbox-theme
   :ensure t
 
+  :init
+  (add-to-list
+   'default-frame-alist
+   '(font . "-xos4-xos4 Terminus-normal-normal-normal-*-12-*-*-*-c-60-iso10646-1"))
+
   :config
-  (load-theme 'habamax t)
-  (set-face-attribute 'default nil :height 170)
-  (set-frame-font "Fira Code Medium"))
+  (load-theme 'gruvbox-dark-hard t)
+  (set-face-attribute 'default nil :height 120)
+  (set-frame-font "Terminus (TTF) Medium"))
