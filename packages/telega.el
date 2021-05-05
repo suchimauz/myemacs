f4378b21bcc2ab437b7cd488cb47ba202c135426
panthevm <panthevm@yandex.ru>
2021-05-05 09:00:21 +0300

    Add supporting telegram emoji

--- packages/telega.el
+++ packages/telega.el
@@ -1,15 +1,24 @@
 (use-package telega :defer t
+  :commands (telega)
 
   :load-path
   "~/.emacs.d/plugins/telega.el"
 
-  :commands
-  (telega)
+  :config
+  (require 'telega-mnz)
+  (setq
+   telega-use-images        t
+   telega-emoji-font-family 'noto-emoji
+   telega-chat-show-deleted-messages-for '(not saved-messages))
+  (telega-notifications-mode 1)
+  (global-telega-mnz-mode 1)
 
   :bind
   (:map main-key
 	;; Main
 	("t d" . telega)
+
+	("t m c" . telega-mnz-chatbuf-attach-code)
 
 	;; Edit
 	("t e p" . telega-chatbuf-edit-prev)
@@ -18,5 +27,4 @@
 	("t a c" . telega-chatbuf-attach-clipboard)
 	("t a p" . telega-chatbuf-attach-photo)
 	("t a v" . telega-chatbuf-attach-voice-note)
-	("t a m" . telega-chatbuf-attach-media)
-	))
+	("t a m" . telega-chatbuf-attach-media)))
